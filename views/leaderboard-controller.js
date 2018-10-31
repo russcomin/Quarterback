d83f002ff3391c3f6218d9ba356bcefe429aa4e4
unknown <jovanbelic234@gmail.com>
2018-10-31 14:46:22 +0100

    Fixes and Animations

--- views/leaderboard-controller.js
+++ views/leaderboard-controller.js
@@ -5,3 +5,7 @@ function match_page(argument) {
 function club_lb(argument) {
 	document.location = "club-leaderboard.html";
 }
+
+function leaderboard_page(argument) {
+	document.location = "leaderboard-v2.html";
+}
